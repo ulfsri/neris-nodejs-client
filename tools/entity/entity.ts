753250f9ff3ea73d9c8c3f19e3910e058820fc50
Tommy Messbauer <thomas.messbauer@ul.org>
2025-02-10 11:58:07 -0600

    Entity - Update Geometry (#9)
    * adds more robust CLI flags for entity tool. implements update_geo action which parses shapefiles, then calls PATCH entity
    
    * PATCH entity is an overwrite of region sets. This adds logic to overwrite when the name of the region_set matches the -n flag; add a new region_set when a region_set with the name does not exist
    
    * remove default for region type. update success message
    
    * adds neris id prefix check to reject entities that do not support geometry
    
    * adds error for unsupported geometry
    
    * implements splicing logic for region upsert. added region delete cli tool. re-organized the entity cli tool to support expansion
    
    * merge changes to support package publishing
    
    * use release instead of publish to tag and version the lib

--- tools/entity/entity.ts
+++ tools/entity/entity.ts
@@ -1,32 +1,13 @@
 #!/usr/bin/env tsx
-import { createNerisClient } from '@api-client/client';
-import chalk from 'chalk';
+import { program } from 'commander';
 import 'dotenv/config';
+import { fetchEntity } from './fetch';
+import { removeRegion, upsertRegion } from './region';
 
-const [_, scriptName, neris_id_entity] = process.argv;
+program.name('Entity CLI tool').description('Performs common tasks on entities using the NERIS api');
 
-const usageMsg = `
-Usage:
-${scriptName} [Entity ID]
+fetchEntity(program);
+upsertRegion(program);
+removeRegion(program);
 
-Example:
-${scriptName} FD24027077`;
-
-if (!neris_id_entity) {
-  console.log(chalk.bold.red('\n⚠️\tEntity ID is required\t⚠️ \n'));
-  console.error(usageMsg);
-  process.exit(1);
-}
-
-const client = createNerisClient();
-const { data, error } = await client.GET(`/entity/{neris_id_entity}`, {
-  params: { path: { neris_id_entity } },
-});
-
-if (error) {
-  console.log(chalk.dim(error.detail));
-  console.log(chalk.bold.red('\n⚠️\tError\t⚠️\n'));
-} else {
-  console.log(chalk.dim(JSON.stringify(data, null, 2)));
-  console.log(chalk.bold.green('\n✨\tSuccess\t✨ \n'));
-}
+program.parse();
